b7e5903d81ca30e28411f6a5d3b16ca934c5bf3a
Hamza Afridi <hamzaafridi@gmail.com>
2020-01-02 09:36:50 +0000

    changes to blog

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -8,3 +8,35 @@ from flaskr.db import get_db
 
 bp = Blueprint('blog', __name__)
 
+@bp.route('/')
+def index():
+    db = get_db()
+    posts = db.execute(
+       'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
+    ).fetchall()
+
+    return render_template('blog/index.html',posts=posts)
+
+@bp.route('/create', methods=('GET','POST'))
+@login_required
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required!'
+        else:
+            db = get_db()
+            db.execute(
+                'INSET INTO post (title, body, author_id)'
+                ' VALUES (?,?,?)',
+                (title,body,g.user['id'],)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+        flash(error)
+    return render_template('blog/create.html')
